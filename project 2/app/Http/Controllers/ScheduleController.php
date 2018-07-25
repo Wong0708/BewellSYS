72b06f4d499ac2aa1b74d2e1fb5e83131a1dfdc3
Kenneth Wong <kenneth_wong@dlsu.edu.ph>
2018-07-25 16:40:30 +0800

    WHOA

--- project 2/app/Http/Controllers/ScheduleController.php
+++ project 2/app/Http/Controllers/ScheduleController.php
@@ -62,7 +62,7 @@ class ScheduleController extends Controller
             $order['locations']= DB::table('bc_client_location')->where('id', $order['clientID'])->get()->toArray();
             $order['order_details']=DB::table("bc_client_order_detail")->where('orderID', $order['id'])->get()->toArray();
             $a = DB::table('bc_client')->select('bc_client.cl_name')->where('id', $order['clientID'])->first();
-            $order['client_name'] = $a->cl_name;
+            $order['client_name'] = "jj";
         }
         // get order and get the client from that order
         // display their list of addresses
