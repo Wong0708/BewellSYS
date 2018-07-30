c4ef851b80d9aeed104e4567953133a7a7a2167c
Fernie22 <fernan_paylago@dlsu.edu.ph>
2018-07-31 02:06:34 +0800

    doggy

--- project 2/resources/views/appdev/manufacturerreport.blade.php
+++ project 2/resources/views/appdev/manufacturerreport.blade.php
@@ -304,11 +304,13 @@
                                         <input type="date" class="form-control" name="end"/> 
                                     </div>
                                 {!!Form::close() !!}
+                            <!-- printhead -->
+                            <div id="printhead">
                                 <p class="text-muted m-b-30"></p>
                                 @if(isset($start))
                                 @endif
                                 @if(isset($end))
-                                <h4  style="text-align:center; font-size:14px; color:black; font-family:Helvetica,Arial,sans-serif;"><b>Bewell-C Nutraceutical Corporation</b></h4>
+                                <h4  style="text-align:center; font-size:14px; color:black; font-family:Helvetica,Arial,sans-serif;"><b>Bewell Nutraceuticals Corporation</b></h4>
                                 <h4  style="text-align:center; font-size:14px; color:black; font-family:Helvetica,Arial,sans-serif;"><b>Sales Report</b></h4>
                                 <h4  style="text-align:center; font-size:14px; color:black; font-family:Helvetica,Arial,sans-serif;"><b>Start date: {{$start}} to end date: {{$end}}  </b></h4>
                                 @endif
@@ -448,8 +450,7 @@
                                                 </table>
                                                 
                                 <h4  style="text-align:center; font-size:14px; color:black; margin-top:20px; font-family:Helvetica,Arial,sans-serif;"><b>----------- END OF THE REPORT -----------</b></h4>
-                                                
-                                
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -539,7 +540,15 @@
             
             <script>
                 function myFunction() {
-                    window.print();
+                    var prtContent = document.getElementById("printhead");
+                    var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+                    WinPrint.document.write(prtContent.innerHTML);
+                    WinPrint.document.close();
+                    WinPrint.focus();
+                    WinPrint.print();
+                    WinPrint.close();
+
+                    //window.print();
                 }
             </script>
 
