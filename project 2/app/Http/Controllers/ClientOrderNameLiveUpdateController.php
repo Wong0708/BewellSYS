966e87ac35e33d2b901138a5240ec40e05669f5a
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-10 18:29:32 +0800

    with error commit

--- project 2/app/Http/Controllers/ClientOrderNameLiveUpdateController.php
+++ project 2/app/Http/Controllers/ClientOrderNameLiveUpdateController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\ProductDetails;
 use App\Material;
 
 class ClientOrderNameLiveUpdateController extends Controller
@@ -27,11 +28,17 @@ class ClientOrderNameLiveUpdateController extends Controller
 
         $materialNameList =  [];
 
-        $productMaterialList = 
+        $productMaterialList = ProductDetails::where('pd_id','=',$product->id)->get();
+        if(isset($productMaterialList)){
+            foreach($i=0;$i<count($productMaterialList);$i=$i+1){
+
+            }
+        }
         $push = array(
             
-         );
-         array_push($materialList,$push);
+        );
+
+        array_push($materialList,$push);
         return response()->json([
             'materialList'=>$materialNameList,
         ]);
