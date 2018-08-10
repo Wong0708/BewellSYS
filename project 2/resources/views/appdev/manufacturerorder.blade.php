2b38c898624490e865d4b90908df90f23ebfd08e
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-10 19:10:05 +0800

    commit

--- project 2/resources/views/appdev/manufacturerorder.blade.php
+++ project 2/resources/views/appdev/manufacturerorder.blade.php
@@ -802,7 +802,7 @@
     $(document).on('change', '.orderName2', function (e) {
         var updatedList = [];
         for(var i = 1; i <=count3; i++){//quick
-            var holder = $('xorderListNum'+i).find('td:first').next().find('select').val();
+            var holder = $('xorderListNum'+i).find('td:first').text();
             alert(holder);
             if(holder!='Choose a product'){
                 updatedList.push(holder);
