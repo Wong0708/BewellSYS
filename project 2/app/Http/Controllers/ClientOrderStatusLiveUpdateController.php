9b42746dbd72e0e07e5b532fc161439914ce7587
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-10 17:10:51 +0800

    well played

--- project 2/app/Http/Controllers/ClientOrderStatusLiveUpdateController.php
+++ project 2/app/Http/Controllers/ClientOrderStatusLiveUpdateController.php
@@ -11,17 +11,21 @@ class ClientOrderStatusLiveUpdateController extends Controller
 {
     public function liveUpdate(Request $request)
     {
+        //Variable Initialization for Order;
+        $date = new DateTime();
+
         //Logs for the order.
-        $order = new ClientOrder();
         $logs = new ClientOrderLogs();
         $logs->userID= auth()->user()->id;
-        $logs->query_date= date('Y-m-d H:i:s');
+        $logs->query_date=  date('Y-m-d H:i:s');
         $logs->query_type= 'Insert';
-        $logs->notification= 'Expected Order Deadline has been updated to '.$request->expectedDate. ' from  '.$order->expectedDate. '!';
+        $logs->notification= 'Expected Order Deadline has been updated to '.$request->expectedDate.'!';
         $logs->created_at= $date->getTimestamp();
         $logs->updated_at= $date->getTimestamp();
         $logs->save();
 
+        //Order Logic Proper
+        $order = new ClientOrder();
         $order = ClientOrder::where('id', $request->id)->first();
         $order->expectedDate = $request->expectedDate;
         $order->save();
