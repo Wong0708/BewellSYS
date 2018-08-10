d8bf32b0edcfb01a21f6100d7aef882e6c6b146f
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-10 17:00:22 +0800

    helloworld

--- project 2/app/Http/Controllers/ClientOrderPaymentLiveUpdateController.php
+++ project 2/app/Http/Controllers/ClientOrderPaymentLiveUpdateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\ClientOrderPayment;
 use App\ClientOrderDetail;
+use App\ClientOrderLogs;
 use App\ClientOrder;
 use DateTime;
 
@@ -13,13 +14,13 @@ class ClientOrderPaymentLiveUpdateController extends Controller
     public function liveUpdate(Request $request)
     {
         $date = new DateTime();
-        $orderPayment = new ClientOrderPayment();
         $orderPayment->orderID = $request->orderID;
         $orderPayment->payment_date = date("Y-m-d");
         $orderPayment->payment_type = $request->paymentType;
         $orderPayment->totalAmount = $request->paymentAmount;
         $orderPayment->save();
 
+        // For processing on the view side.
         $orders = ClientOrderDetail::where('orderID','=',$request->orderID)->get();
         $orderPayments = ClientOrderPayment::where('orderID','=',$request->orderID)->get();
         $totalOrder = 0;
@@ -36,6 +37,16 @@ class ClientOrderPaymentLiveUpdateController extends Controller
                 $totalPayment = $totalPayment + $orderPayment->totalAmount;
             }
         }
+
+        $logs = new ClientOrderLogs();
+        $logs->userID= auth()->user()->id;
+        $logs->query_date= date('Y-m-d H:i:s');
+        $logs->query_type= 'Insert';
+        $logs->notification=  $request->paymentType.' Payment Amount of PHP'.  $request->paymentType.' has been added!';
+        $logs->created_at= $date->getTimestamp();
+        $logs->updated_at= $date->getTimestamp();
+        $logs->save();
+
 
         return response()->json([
             'totalBalance'=>$totalOrder-$totalPayment,
