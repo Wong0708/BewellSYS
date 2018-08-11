d0d8b44224dccc76c86e8c0bdf3ae147b4594432
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-11 22:51:50 +0800

    update model for supplier logs

--- project 2/app/SupplierOrderLogs.php
+++ project 2/app/SupplierOrderLogs.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class SupplierOrderLogs extends Model
 {
     protected $table="BC_Supplier_Order_Logs";
+    
+    public function fromUser(){
+        return $this->belongsTo(User::class,'userID','id');
+    }
 }
