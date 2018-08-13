fbe5a949396a6266565d874d331365aab568f369
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-14 06:59:04 +0800

    updated the a file for scheduling

--- project 2/app/Http/Controllers/TruckCapacityLiveUpdateController.php
+++ project 2/app/Http/Controllers/TruckCapacityLiveUpdateController.php
@@ -11,16 +11,20 @@ use App\ScheduleDetail;
 class TruckCapacityLiveUpdateController extends Controller
 {
     public function liveUpdate(Request $request)
-    {   
+    {      
         $truck = Truck::where('plate_num','=',$request->truckPlateNumber)->first();
         $schedules = Schedule::where('scd_date','=',$request->deliveryDate)
                                 ->where('truckID','=',$truck->id)
                                 ->get();
+
         $truckCapacity = $truck->max_box;
         $totalDelivery = 0; 
-        foreach($schedules as $schedule){
-            foreach($schedule->fromScheduleDetail as $scheduleInfo){
-                $totalDelivery = $totalDelivery+$scheduleInfo->delivered_qty;
+
+        if(count($schedules)>0){
+            foreach($schedules as $schedule){
+                foreach($schedule->fromScheduleDetail as $scheduleInfo){
+                    $totalDelivery = $totalDelivery+$scheduleInfo->delivered_qty;
+                }
             }
         }
 
