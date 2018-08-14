4aa591ff07b8fdc3c9dfe48e8ad9cfde4a0def1a
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-14 09:02:43 +0800

    edited dashboard controller

--- project 2/app/Http/Controllers/DashboardController.php
+++ project 2/app/Http/Controllers/DashboardController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Schedule;
 use App\Product;
+use DB;
 
 class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illumi
+     * @return \Illuminate
      *
      * nate\Http\Response
      */
@@ -24,8 +25,15 @@ class DashboardController extends Controller
 
     public function index()
     {
-        
-        return view('appdev.dashboard')->with();
+        $deliveries =Schedule::where(([['scd_status','=','Scheduled'],['scd_status','=', 'Processing']]))
+                    ->get();
+
+        $products =Products::where('pd_status','=','Re-stock')
+                    ->get();
+
+        return view('appdev.dashboard')
+                    ->with($deliveries)
+                    ->with($products);
     }
 
     /**
