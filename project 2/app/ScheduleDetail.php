a2f7637816e287a2f083493e9b619444ba506a25
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-14 08:40:24 +0800

    UPDATE FOR SCHEDULE DETAIL

--- project 2/app/ScheduleDetail.php
+++ project 2/app/ScheduleDetail.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class ScheduleDetail extends Model
 {
     protected $table="bc_schedule_detail";
+
+    public function fromProduct(){
+        return $this->belongsTo(Product::class,'productID','id');
+    }
 }
